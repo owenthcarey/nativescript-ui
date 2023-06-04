071c413684d52c80b7004c4850e65251058909c4
owenthcarey <owenthcarey@gmail.com>
2023-06-04 15:43:01 -0700

    Update home module

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core'
+import {Color} from "~/app/core/models/color.model";
+import {ColorService} from "~/app/core/services/color.service";
 
 @Component({
   moduleId: module.id,
   selector: 'ns-home',
   templateUrl: 'home.component.html'
 })
-export class HomeComponent {}
+export class HomeComponent {
+  colors: Color[];
+
+  constructor(private colorService: ColorService) {
+    this.colors = this.colorService.generateRandomColors(50);
+  }
+}
