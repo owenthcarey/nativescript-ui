7da8e17aa2371f248e85533a8524c9b3f0ff644d
owenthcarey <owenthcarey@gmail.com>
2023-06-04 19:00:06 -0700

    Update notifications module

--- src/app/features/notifications/notifications.module.ts
+++ src/app/features/notifications/notifications.module.ts
@@ -2,9 +2,12 @@ import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core'
 import { NativeScriptCommonModule } from '@nativescript/angular'
 import { NotificationsRoutingModule } from './notifications-routing.module'
 import { NotificationsComponent } from './notifications.component'
+import {
+  CollectionViewModule
+} from "@nativescript-community/ui-collectionview/angular";
 
 @NgModule({
-  imports: [NativeScriptCommonModule, NotificationsRoutingModule],
+  imports: [NativeScriptCommonModule, NotificationsRoutingModule, CollectionViewModule],
   declarations: [NotificationsComponent],
   schemas: [NO_ERRORS_SCHEMA]
 })
