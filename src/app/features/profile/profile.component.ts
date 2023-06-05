d3887698e669511f1b8f7400921e2d92f0ec7dc0
owenthcarey <owenthcarey@gmail.com>
2023-06-04 19:26:55 -0700

    Update profile module

--- src/app/features/profile/profile.component.ts
+++ src/app/features/profile/profile.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core'
+import {Color} from "~/app/core/models/color.model";
+import {ColorService} from "~/app/core/services/color.service";
 
 @Component({
   moduleId: module.id,
   selector: 'ns-profile',
   templateUrl: 'profile.component.html'
 })
-export class ProfileComponent {}
+export class ProfileComponent {
+  colors: Color[];
+
+  constructor(private colorService: ColorService) {
+    this.colors = this.colorService.generateRandomColors(50);
+  }
+
+  templateSelector(item: Color, index: number, items: Color[]) {
+    return index === 0 ? 'header' : 'item';
+  }
+
+  spanSize(index: number) {
+    return index === 0 ? 3 : 1; // Return 3 for the first item and 1 for the rest
+  }
+}
--- src/app/features/profile/profile.module.ts
+++ src/app/features/profile/profile.module.ts
@@ -2,9 +2,12 @@ import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core'
 import { NativeScriptCommonModule } from '@nativescript/angular'
 import { ProfileRoutingModule } from './profile-routing.module'
 import { ProfileComponent } from './profile.component'
+import {
+  CollectionViewModule
+} from "@nativescript-community/ui-collectionview/angular";
 
 @NgModule({
-  imports: [NativeScriptCommonModule, ProfileRoutingModule],
+  imports: [NativeScriptCommonModule, ProfileRoutingModule, CollectionViewModule],
   declarations: [ProfileComponent],
   schemas: [NO_ERRORS_SCHEMA]
 })
